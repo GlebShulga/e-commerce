009b75c833f1c108f36ef6418b2f5beeacf1e4ec
GlebShulga <shulga_gleb@hotmail.com>
2021-01-07 09:33:58 +1000

    add conditions for currency buttons

--- client/components/currency.js
+++ client/components/currency.js
@@ -1,31 +1,33 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { setCurrentRate } from '../redux/reducers/goods'
 
 const Currency = () => {
   const dispatch = useDispatch()
+  const currencyType = useSelector((s) => s.goods.currency)
   const buttonClassName =
     'border border-blue-500 text-blue-500 rounded-md px-4 py-2 mx-2 transition duration-500 ease select-none hover:text-white hover:bg-blue-600 focus:outline-none focus:shadow-outline'
+  const pushedButtonClassName = 'text-white bg-blue-600 shadow-outline rounded-md px-4 py-2 mx-2'
   return (
     <div>
       <button
         type="button"
-        className={buttonClassName}
+        className={currencyType === 'USD' ? pushedButtonClassName : buttonClassName}
         onClick={() => dispatch(setCurrentRate('USD'))}
       >
         USD
       </button>
       <button
         type="button"
-        className={buttonClassName}
+        className={currencyType === 'EUR' ? pushedButtonClassName : buttonClassName}
         onClick={() => dispatch(setCurrentRate('EUR'))}
       >
         EUR
       </button>
       <button
         type="button"
-        className={buttonClassName}
+        className={currencyType === 'CAD' ? pushedButtonClassName : buttonClassName}
         onClick={() => dispatch(setCurrentRate('CAD'))}
       >
         CAD
