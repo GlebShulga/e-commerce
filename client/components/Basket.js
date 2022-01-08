2715c7af0bbdd421b9e034635183176ab7d87027
GlebShulga <shulga_gleb@hotmail.com>
2022-01-08 15:32:12 +0300

    Basket refactoring 2.0

--- client/components/Basket.js
+++ client/components/Basket.js
@@ -51,33 +51,6 @@ const Basket = () => {
                     <tbody key={`${title} + ${price} + ${quantity}`}>
                       <ItemInBasket item={item} />
                     </tbody>
-                    // <tbody key={`${title} + ${price} + ${quantity}`}>
-                    //   <tr>
-                    //     <td className="hidden pb-4 md:table-cell">
-                    //       <img
-                    //         alt={title}
-                    //         className="product__image block object-cover h-24 w-24"
-                    //         src={image}
-                    //       />
-                    //     </td>
-                    //     <td className="product__title mb-2 md:ml-4">{title}</td>
-                    //     <td className="md:justify-end md:flex mt-6">
-                    //       <AddToBasket itemQuantityInBasket={quantity} it={item} />
-                    //     </td>
-                    //     <td className="hidden text-right md:table-cell">
-                    //       <span className="product__price mx-1 text-sm lg:text-base font-medium">
-                    //         {(price * currentRate).toFixed(2)}
-                    //       </span>
-                    //       <span className="text-sm lg:text-base font-medium">{currencyType}</span>
-                    //     </td>
-                    //     <td className="text-right">
-                    //       <span className="product__total_price mx-1 text-sm lg:text-base font-medium">
-                    //         {(quantity * price * currentRate).toFixed(2)}
-                    //       </span>
-                    //       <span className="text-sm lg:text-base font-medium">{currencyType}</span>
-                    //     </td>
-                    //   </tr>
-                    // </tbody>
                   )
                 })}
               </table>
