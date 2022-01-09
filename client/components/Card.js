683bd69c68aae58111af0803ed8c687a7b15eb3d
GlebShulga <shulga_gleb@hotmail.com>
2022-01-09 15:20:34 +0300

    Card component refactoring

--- client/components/Card.js
+++ client/components/Card.js
@@ -5,22 +5,18 @@ import { changeItemQuantityInBasket } from '../redux/reducers/basket'
 
 const Card = ({ it, currentRate, currencyType, itemQuantityInBasket }) => {
   const dispatch = useDispatch()
+  const { id, title, image, price } = it
   return (
     <article className="overflow-hidden rounded-lg shadow-lg">
-      <img
-        alt={it.title}
-        className="block object-cover h-64 w-full"
-        src={it.image}
-        loading="lazy"
-      />
+      <img alt={title} className="block object-cover h-64 w-full" src={image} loading="lazy" />
       <header className="flex items-center justify-between leading-tight p-2 md:p-4">
         <h1 className="text-lg">
-          <div className="no-underline hover:underline text-black">{it.title}</div>
+          <div className="no-underline hover:underline text-black">{title}</div>
         </h1>
       </header>
       <div className="container mx-auto flex justify-between p-5 flex-col md:flex-row items-center sm:px-2">
         <div>
-          <span className="mx-1">{(it.price * currentRate).toFixed(2)}</span>
+          <span className="mx-1">{(price * currentRate).toFixed(2)}</span>
           <span>{currencyType}</span>
         </div>
         <div className="sm:px-2">
@@ -29,7 +25,7 @@ const Card = ({ it, currentRate, currencyType, itemQuantityInBasket }) => {
               id="cart button"
               type="button"
               className="flex ml-auto text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded focus-within:bg-gray-500"
-              onClick={() => dispatch(changeItemQuantityInBasket(it.id, 1))}
+              onClick={() => dispatch(changeItemQuantityInBasket(id, 1))}
             >
               Add to cart
             </button>
